23dd427c5c8ecdd80f900bd199665940aded1adb
Zach Drummond <zachdrummond3@gmail.com>
2021-08-17 16:08:27 -0400

    Added GitHub icon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import bootstrap from 'bootstrap'
 
 ReactDOM.render(
   <React.StrictMode>
