07e87dd735e33de2c73b55e30b04fc138fa321c0
Zach Drummond <zachdrummond3@gmail.com>
2021-09-08 17:02:03 -0400

    Changed Portfolio folder to Projects folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import About from "./Containers/About/About.jsx"
 import Navbar from "./Containers/Navbar/Navbar.jsx";
-import Projects from "./Containers/Portfolio/Projects.jsx";
+import Projects from "./Containers/Projects/Projects.jsx";
 import Footer from "./Containers/Footer/Footer.jsx";
 
 function App() {
