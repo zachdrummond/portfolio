77f67a999666d8cc710f691b2ffbf4ee53a7ed58
Zach Drummond <zachdrummond3@gmail.com>
2021-09-09 16:14:08 -0400

    Added App.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import About from "./Containers/About/About.jsx"
-import Contact from "./Containers/Contact/Contact.jsx"
+import About from "./Containers/About/About.jsx";
+import Contact from "./Containers/Contact/Contact.jsx";
 import Navbar from "./Containers/Navbar/Navbar.jsx";
 import Projects from "./Containers/Projects/Projects.jsx";
 import Footer from "./Containers/Footer/Footer.jsx";
 
 function App() {
   return (
-    <>
+    <div id="page-container">
       <Navbar />
-      <About />
-      <Projects/>
-      <Contact />
+      <div id="content-wrap">
+        <About />
+        <Projects />
+        <Contact />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/Containers/Navbar/Navbar.jsx
+++ src/Containers/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import NavLinks from "../../Components/NavLinks/NavLinks.jsx";
 // The Container for the Navbar
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
       <Name/>
       <NavLinks/>
     </nav>
