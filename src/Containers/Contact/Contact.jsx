d7013ad7285a378af8033eac4ad68b3e5737d5cb
Zach Drummond <zachdrummond3@gmail.com>
2021-09-15 11:06:59 -0400

    updated Contact

--- src/Containers/Contact/Contact.jsx
+++ src/Containers/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import "./Contact.css";
 import { useForm } from "react-hook-form";
 import emailjs from "emailjs-com";
+import Section from "../../Components/Section/Section.jsx";
 
 const Contact = () => {
   const {
@@ -30,104 +31,91 @@ const Contact = () => {
   };
 
   return (
-    <main className="container">
-      <div className="row justify-content-center">
-        <div className="card col-md-12 mt-5 bg-light">
-          <div className="card-body">
-            <div className="row" id="row1">
-              {/* Header - Contact */}
-              <div className="col-sm-12" id="Header">
-                <h2 className="header fw-bold font">Contact</h2>
-              </div>
-            </div>
-            <hr />
-            {/* Form */}
-            <form className="row" id="row2" onSubmit={handleSubmit(onSubmit)}>
-              {/* Form - Name */}
-              <div className="col-sm-12 pb-2" id="Name">
-                <div className="form-floating">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    name="name"
-                    placeholder="Name"
-                    aria-describedby="Name"
-                    {...register("name", {
-                      required: {
-                        value: true,
-                        message: "Please enter your name",
-                      },
-                      maxLength: {
-                        value: 30,
-                        message: "Please use 30 characters or less",
-                      },
-                    })}
-                  />
-                  {errors.name && (
-                    <span className="errorMessage">{errors.name.message}</span>
-                  )}
-                  <label htmlFor="username" className="form-label">
-                    Name
-                  </label>
-                </div>
-              </div>
-              {/* Form - Email */}
-              <div className="col-sm-12 pb-2" id="Email">
-                <div className="form-floating">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="userEmail"
-                    name="email"
-                    placeholder="Email"
-                    aria-describedby="Email"
-                    {...register("email", {
-                      required: true,
-                      pattern:
-                        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                    })}
-                  />
-                  {errors.email && (
-                    <span className="errorMessage">
-                      Please enter a valid email address
-                    </span>
-                  )}
-                  <label htmlFor="userEmail" className="form-label">
-                    Email
-                  </label>
-                </div>
-              </div>
-              {/* Form - Message */}
-              <div className="col-sm-12 pb-2" id="Message">
-                <div className="form-floating">
-                  <textarea
-                    className="form-control"
-                    id="userMessage"
-                    name="message"
-                    placeholder="Message"
-                    aria-describedby="Message"
-                    {...register("message", {
-                      required: true,
-                    })}
-                  />
-                  {errors.message && (
-                    <span className="errorMessage">Please enter a message</span>
-                  )}
-                  <label htmlFor="userMessage">Message</label>
-                </div>
-              </div>
-              {/* Form - Button */}
-              <div className="col-sm-12" id="submitButton">
-                <button type="submit" className="btn btn-primary color">
-                  Submit
-                </button>
-              </div>
-            </form>
+    <Section title="Contact">
+      {/* Form */}
+      <form className="row" id="row2" onSubmit={handleSubmit(onSubmit)}>
+        {/* Form - Name */}
+        <div className="col-sm-12 pb-2" id="Name">
+          <div className="form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="name"
+              placeholder="Name"
+              aria-describedby="Name"
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Please enter your name",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "Please use 30 characters or less",
+                },
+              })}
+            />
+            {errors.name && (
+              <span className="errorMessage">{errors.name.message}</span>
+            )}
+            <label htmlFor="username" className="form-label">
+              Name
+            </label>
           </div>
         </div>
-      </div>
-    </main>
+        {/* Form - Email */}
+        <div className="col-sm-12 pb-2" id="Email">
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="userEmail"
+              name="email"
+              placeholder="Email"
+              aria-describedby="Email"
+              {...register("email", {
+                required: true,
+                pattern:
+                  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              })}
+            />
+            {errors.email && (
+              <span className="errorMessage">
+                Please enter a valid email address
+              </span>
+            )}
+            <label htmlFor="userEmail" className="form-label">
+              Email
+            </label>
+          </div>
+        </div>
+        {/* Form - Message */}
+        <div className="col-sm-12 pb-2" id="Message">
+          <div className="form-floating">
+            <textarea
+              className="form-control"
+              id="userMessage"
+              name="message"
+              placeholder="Message"
+              aria-describedby="Message"
+              {...register("message", {
+                required: true,
+              })}
+            />
+            {errors.message && (
+              <span className="errorMessage">Please enter a message</span>
+            )}
+            <label htmlFor="userMessage">Message</label>
+          </div>
+        </div>
+        {/* Form - Button */}
+        <div className="col-sm-12" id="submitButton">
+          <button type="submit" className="btn btn-primary color">
+            Submit
+          </button>
+        </div>
+      </form>
+    </Section>
   );
 };
 
