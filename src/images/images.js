13283a946d310a5f1822430dca828b98fa81e82f
Zach Drummond <zachdrummond3@gmail.com>
2021-09-16 12:57:54 -0400

    Added all techincal skills logos

--- src/images/images.js
+++ src/images/images.js
@@ -1,11 +1,23 @@
 const IMAGES = {
-    BudgetTracker: require("./Portfolio/BudgetTracker.png").default,
-    Ciphr: require("./Portfolio/Ciphr.png").default,
-    EatDaBurger: require("./Portfolio/Eat-da-Burger.png").default,
-    FindYourAdventure: require("./Portfolio/FindYourAdventure.png").default,
-    Gifted: require("./Portfolio/Gifted.png").default,
-    GoogleBooksSearch: require("./Portfolio/GoogleBooksSearch.png").default,
     Headshot: require("./Headshot.jpg").default,
-    HTML: require("./Skills/HTML.png").default,
+    Portfolio: {
+        BudgetTracker: require("./Portfolio/BudgetTracker.png").default,
+        Ciphr: require("./Portfolio/Ciphr.png").default,
+        EatDaBurger: require("./Portfolio/Eat-da-Burger.png").default,
+        FindYourAdventure: require("./Portfolio/FindYourAdventure.png").default,
+        Gifted: require("./Portfolio/Gifted.png").default,
+        GoogleBooksSearch: require("./Portfolio/GoogleBooksSearch.png").default,
+    },
+    Skills: {
+        CSS: require("./Skills/CSS.svg").default,
+        Expressjs: require("./Skills/Expressjs.svg").default,
+        Git: require("./Skills/Git.svg").default,
+        HTML: require("./Skills/HTML.svg").default,
+        Javascript: require("./Skills/Javascript.svg").default,
+        MongoDB: require("./Skills/MongoDB.svg").default,
+        MySQL: require("./Skills/MySQL.svg").default,
+        Nodejs: require("./Skills/Nodejs.svg").default,
+        React: require("./Skills/React.svg").default,
+    },
 }
 export default IMAGES;
