ab4f43e8ec11fe7d580fc875fe32737f31cdf3e4
Zach Drummond <zachdrummond3@gmail.com>
2021-09-22 10:55:21 -0400

    Added conditionals to carousel.jsx

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -25,24 +25,37 @@ const Carousel = ({ id, images, alt }) => {
           ""
         )}
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target={`#carousel-${id}`}
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target={`#carousel-${id}`}
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+      {images.two ? (
+        <>
+          {" "}
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target={`#carousel-${id}`}
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target={`#carousel-${id}`}
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>{" "}
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
