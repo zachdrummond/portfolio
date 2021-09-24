c19b717efb497f5a58da4cde792abed7177d60e1
Zach Drummond <zachdrummond3@gmail.com>
2021-09-24 16:55:12 -0400

    Added width to contact.jsx

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,7 +1,7 @@
 const Section = ({id, name, children}) => {
   return (
-    <section id={id} className="container mt-5 text-center">
-      <h1>{name}</h1>
+    <section id={id} className="container my-5 text-center">
+      <h1 className="mb-5">{name}</h1>
       {children}
     </section>
   );
