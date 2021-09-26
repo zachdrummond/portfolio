0d046d9cb9328c3a595a61c0cc7d74dbc52b9875
Zach Drummond <zachdrummond3@gmail.com>
2021-09-26 11:20:28 -0400

    Added headshot to About.jsx

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,22 +1,27 @@
 import "./About.css";
-// import IMAGES from "../../images/images";
+import IMAGES from "../../images/images";
 import TechSkills from "../../Components/TechSkills/TechSkills";
 import Section from "../../Components/Section/Section";
 
 const About = () => {
   return (
     <Section id="About" name="Hey, I'm Zach!">
-      {/* <img
-        className="img-fluid img-thumbnail"
-        src={IMAGES.Headshot}
-        alt="Head Shot of Zach Drummond"
-        id="aboutMeImg"
-        height="90"
-      /> */}
-      <h2>
-        I'm a full stack web developer with a passion for developing {<br />}
-        mobile-first, user-friendly apps that solve real world needs.
-      </h2>
+      <div className="d-flex row align-items-center">
+        <div className="col-2">
+          <img
+            className="img-fluid img-thumbnail"
+            src={IMAGES.Headshot}
+            alt="Head Shot of Zach Drummond"
+          />
+        </div>
+        <div className="col-10">
+          <h2>
+            I'm a full stack web developer with a passion for developing
+            {<br />}
+            mobile-first, user-friendly apps that solve real world needs.
+          </h2>
+        </div>
+      </div>
       <TechSkills />
     </Section>
   );
