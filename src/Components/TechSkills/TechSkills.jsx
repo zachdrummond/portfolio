80debe09d256d2f7550c3b4b552dfb0301c9daa6
Zach Drummond <zachdrummond3@gmail.com>
2021-09-27 11:29:47 -0400

    Added comments to TechSkills.jsx and ProjectGallery.jsx

--- src/Components/TechSkills/TechSkills.jsx
+++ src/Components/TechSkills/TechSkills.jsx
@@ -1,14 +1,27 @@
 import IMAGES from "../../images/images";
 import Section from "../../Components/Section/Section";
 
+// SECTION - About
+// SUMMARY - Displays all technical skills by mapping over the images and data in images.js
 const TechSkills = () => {
+  // SUMMARY
+  //    1. Creates a container (<div>)
+  //    2. Object.keys(object) returns an array of a the object's property names
+  //    3. .map(key) iterates over the array of property names
+  //    4. A container (<div>) with the object's image and header is added to each element in the array
+  //    5. A new array is created, returned, and added to the container (<div>)
+  // ARGUMENTS - object (type - object)
+  // RETURNS - A new object (React Element <div>)
   const objectMap = (object) => {
     return (
       <div className="d-flex justify-content-center">
         {Object.keys(object).map((key) => {
           let { name, image, alt } = object[key];
           return (
-            <div className="col-2 border border-3 border-dark rounded-pill m-3 p-1" key={key}>
+            <div
+              className="col-2 border border-3 border-dark rounded-pill m-3 p-1"
+              key={key}
+            >
               <img className="skill-img img-fluid" src={image} alt={alt} />
               <h5>{name}</h5>
             </div>
--- src/Containers/ProjectGallery/ProjectGallery.jsx
+++ src/Containers/ProjectGallery/ProjectGallery.jsx
@@ -3,6 +3,14 @@ import IMAGES from "../../images/images";
 import Project from "../../Components/Project/Project";
 import Section from "../../Components/Section/Section";
 
+// SECTION - About
+// SUMMARY
+//    1. Creates a container (<Section>)
+//    2. Creates another container for a row (<div>)
+//    2. Object.keys(object) returns an array of a the object's property names
+//    3. .map(key) iterates over the array of property names
+//    4. A component (<Project>) is added to each element in the array
+//    5. A new array is created, returned, and added to the container (<div>)
 const ProjectGallery = () => {
   return (
     <Section id="Projects" name="Projects">
