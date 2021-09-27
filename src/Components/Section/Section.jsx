491567fad9fdc75dad6fdc277b28ff7a8ab3e140
Zach Drummond <zachdrummond3@gmail.com>
2021-09-27 10:46:23 -0400

    Added comments to Section.jsx

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,6 +1,9 @@
+// SUMMARY - Wraps each section in a container with a header
+// PROPS - All come from a specific section
 const Section = ({ id, name, children }) => {
   return (
     <section id={id} className="container my-5 text-center">
+      {/* Adds < /> to each header */}
       <h1 className="mb-5">&lt;&nbsp;{name}&nbsp;/&gt;</h1>
       {children}
     </section>
