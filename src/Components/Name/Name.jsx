11b501821521b4cefe179fafb9e3962bb20a9a83
Zach Drummond <zachdrummond3@gmail.com>
2021-09-27 09:58:19 -0400

    Updaed Name.jsx and Section.jsx with < />

--- src/Components/Name/Name.jsx
+++ src/Components/Name/Name.jsx
@@ -3,40 +3,9 @@
 const Name = () => {
   return (
     <>
-      <a
-        id="name"
-        className="navbar-brand fw-bold font"
-        href="/"
-      >
-        {/* < Arrow */}
-        <svg
-          width="1.5em"
-          height="1.5em"
-          viewBox="0 0 16 16"
-          className="bi bi-chevron-left"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-          />
-        </svg>
-        Zach Drummond
-        {/* > Arrow */}
-        <svg
-          width="1.5em"
-          height="1.5em"
-          viewBox="0 0 16 16"
-          className="bi bi-chevron-right"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-          />
-        </svg>
+      <a id="name" className="navbar-brand fw-bold font ms-3" href="/">
+        <span>&lt;&nbsp;</span>Z<span>ach&nbsp;</span>D
+        <span>rummond&nbsp;/&gt;</span>
       </a>
       {/* Navbar Collapsible Button for Nav-Items on the right side */}
       <button
--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,7 +1,7 @@
-const Section = ({id, name, children}) => {
+const Section = ({ id, name, children }) => {
   return (
     <section id={id} className="container my-5 text-center">
-      <h1 className="mb-5">{name}</h1>
+      <h1 className="mb-5">&lt;&nbsp;{name}&nbsp;/&gt;</h1>
       {children}
     </section>
   );
