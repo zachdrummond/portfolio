2e9c4005465bf4ee09a66bcd7882f12880df72da
Zach Drummond <zachdrummond3@gmail.com>
2021-12-16 16:52:53 -0500

    Added css clases for Navbar

--- src/images/images.js
+++ src/images/images.js
@@ -2,7 +2,7 @@ const IMAGES = {
   Profiles: {
     Headshot: require("./Profiles/Headshot.jpg"),
     HeadshotCircle: require("./Profiles/HeadshotCircle.jfif"),
-    GlassesHeadshot: require("./Profiles/GlassesHeadshot.jpg").default,
+    GlassesHeadshot: require("./Profiles/GlassesHeadshot.jpg"),
     GlassesHeadshotCircle: require("./Profiles/GlassesHeadshotCircle.jfif"),
   },
   Projects: {
@@ -14,9 +14,9 @@ const IMAGES = {
         "Ciphr is a platform to share and solve coding algorithms. It enables developers to connect with each other in order to strengthen their critical thinking abilities and problem solving skills.",
       images: {
         logo: require("./Projects/Ciphr/Ciphr.png"),
-        one: require("./Projects/Ciphr/HomePage.png").default,
-        two: require("./Projects/Ciphr/ChallengePage.png").default,
-        three: require("./Projects/Ciphr/SolutionPage.png").default,
+        one: require("./Projects/Ciphr/HomePage.png"),
+        two: require("./Projects/Ciphr/ChallengePage.png"),
+        three: require("./Projects/Ciphr/SolutionPage.png"),
       },
       alt: "Ciphr Project",
       link: "https://ciphr.dev/",
@@ -30,8 +30,8 @@ const IMAGES = {
       description:
         "Do you dream about tasty burgers? Eat-da-Burger is an app where you can create any fictional burger you desire and eat it online!",
       images: {
-        logo: require("./Projects//EatDaBurger/BothBurgers.png").default,
-        one: require("./Projects//EatDaBurger/BurgersToEat.png").default,
+        logo: require("./Projects//EatDaBurger/BothBurgers.png"),
+        one: require("./Projects//EatDaBurger/BurgersToEat.png"),
       },
       alt: "Eat-da-Burger Project",
       link: "https://eat-da-burger-zachdrummond.herokuapp.com/",
@@ -45,11 +45,11 @@ const IMAGES = {
       description:
         "Find Your Adventure is a website that gives you everything you need to know about a national park site in one place, based on your preferences. We want to help users like you spend your precious time at one of America’s national park site, not online trying to get there.",
       images: {
-        logo: require("./Projects/FindYourAdventure/HomePage.png").default,
-        one: require("./Projects/FindYourAdventure/ActivitiesPage.png").default,
+        logo: require("./Projects/FindYourAdventure/HomePage.png"),
+        one: require("./Projects/FindYourAdventure/ActivitiesPage.png"),
         two: require("./Projects/FindYourAdventure/ListOfParksPage.png")
-          .default,
-        three: require("./Projects/FindYourAdventure/ParkPage.png").default,
+          ,
+        three: require("./Projects/FindYourAdventure/ParkPage.png"),
       },
       alt: "Find Your Adventure Project",
       link: "https://zachdrummond.github.io/Find-Your-Adventure/",
@@ -63,10 +63,10 @@ const IMAGES = {
       description:
         "Google Books Search is an app that uses Google Books API to find and save any book.",
       images: {
-        logo: require("./Projects/GoogleBooksSearch/HomePage.png").default,
-        one: require("./Projects/GoogleBooksSearch/SavedBooksPage.png").default,
+        logo: require("./Projects/GoogleBooksSearch/HomePage.png"),
+        one: require("./Projects/GoogleBooksSearch/SavedBooksPage.png"),
         two: require("./Projects/GoogleBooksSearch/SearchResultsPage.png")
-          .default,
+          ,
       },
       alt: "Google Books Search Project",
       link: "https://google-books-search-zd.herokuapp.com/search",
@@ -80,10 +80,10 @@ const IMAGES = {
       description:
         "Gifted is an app that tracks all of your gifts in one organized place. There is a table that organizes all of the gifts you receive from others and a table that organizes all of the gifts that you send to others.",
       images: {
-        logo: require("./Projects/Gifted/HomePage.png").default,
-        one: require("./Projects/Gifted/giftsReceivedPage.png").default,
-        two: require("./Projects/Gifted/addReceivedPage.png").default,
-        three: require("./Projects/Gifted/sentGiftsPage.png").default,
+        logo: require("./Projects/Gifted/HomePage.png"),
+        one: require("./Projects/Gifted/giftsReceivedPage.png"),
+        two: require("./Projects/Gifted/addReceivedPage.png"),
+        three: require("./Projects/Gifted/sentGiftsPage.png"),
       },
       alt: "Gifted Project",
       link: "http://gifted-project2.herokuapp.com/",
@@ -97,8 +97,8 @@ const IMAGES = {
       description:
         "Budget Tracker is a progressive web app tracks all of your financial needs in one place. You can access everything at any time, even if you are offline. You can even download this app on your mobile device.",
       images: {
-        logo: require("./Projects/BudgetTracker/Example.png").default,
-        one: require("./Projects/BudgetTracker/HomePage.png").default,
+        logo: require("./Projects/BudgetTracker/Example.png"),
+        one: require("./Projects/BudgetTracker/HomePage.png"),
       },
       alt: "Budget Tracker Project",
       link: "https://pwa-budget-tracker-zd.herokuapp.com/",
@@ -110,49 +110,49 @@ const IMAGES = {
     TopRow: {
       React: {
         name: "REACT.JS",
-        image: require("./Skills/React.svg").default,
+        image: require("./Skills/React.svg"),
         alt: "React Logo",
       },
       Nodejs: {
         name: "NODE.JS",
-        image: require("./Skills/Nodejs.svg").default,
+        image: require("./Skills/Nodejs.svg"),
         alt: "Nodejs Logo",
       },
       Expressjs: {
         name: "EXPRESS.JS",
-        image: require("./Skills/Expressjs.svg").default,
+        image: require("./Skills/Expressjs.svg"),
         alt: "Expressjs Logo",
       },
       MongoDB: {
         name: "MONGO DB",
-        image: require("./Skills/MongoDB.svg").default,
+        image: require("./Skills/MongoDB.svg"),
         alt: "MongoDB Logo",
       },
     },
     BottomRow: {
       HTML: {
         name: "HTML",
-        image: require("./Skills/HTML.svg").default,
+        image: require("./Skills/HTML.svg"),
         alt: "HTML Logo",
       },
       CSS: {
         name: "CSS",
-        image: require("./Skills/CSS.svg").default,
+        image: require("./Skills/CSS.svg"),
         alt: "CSS Logo",
       },
       Javascript: {
         name: "JAVASCRIPT",
-        image: require("./Skills/Javascript.svg").default,
+        image: require("./Skills/Javascript.svg"),
         alt: "JavaScript Logo",
       },
       Git: {
         name: "GIT",
-        image: require("./Skills/Git.svg").default,
+        image: require("./Skills/Git.svg"),
         alt: "Git Logo",
       },
       MySQL: {
         name: "MYSQL",
-        image: require("./Skills/MySQL.svg").default,
+        image: require("./Skills/MySQL.svg"),
         alt: "MySQL Logo",
       },
     },
