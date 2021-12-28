d892d3da733e3a4b4620ded390e51ad2260d82e8
Zach Drummond <zachdrummond3@gmail.com>
2021-12-28 16:29:26 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import "./CSS/debug.css";
 import "./index.css";
 import App from './App';
 import React from 'react';
