61dea38c3ba3a1c047d105b3b91a26cf0affdf14
Zach Drummond <zachdrummond3@gmail.com>
2022-01-06 16:41:12 -0500

    Updated columns in about

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -8,8 +8,8 @@ import Section from "../../Components/Section/Section";
 const About = () => {
   return (
     <Section id="About" name="Hey, I'm Zach!">
-      <div className="d-flex align-items-center justify-content-around">
-        <div className="col-lg-2">
+      <div className="d-flex align-items-center justify-content-center">
+        <div className="col-md-3 col-lg-2">
           <img
             id="headshot"
             className="img-fluid img-thumbnail rounded-circle"
@@ -17,7 +17,7 @@ const About = () => {
             alt="Head Shot of Zach Drummond"
           />
         </div>
-        <div className="col-lg-9">
+        <div className="col-md-9 col-lg-10">
           <h2 className="m-0">
             I'm a software engineer with a passion for consistently achieving
             results through innovative software solutions that users love.
