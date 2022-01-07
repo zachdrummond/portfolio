65527d9295f02eca9ec0d7858a8f5499fa111696
Zach Drummond <zachdrummond3@gmail.com>
2022-01-07 10:16:10 -0500

    Added width to headshot and changed section

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -2,7 +2,7 @@
 // PROPS - All come from a specific section
 const Section = ({ id, grid, name, children }) => {
   return (
-    <section className="my-5">
+    <section className="my-3">
       <div id={id} className={`container text-center ${grid}`}>
         {/* Adds < /> to each header */}
         <h1 className="mb-4">&lt;&nbsp;{name}&nbsp;/&gt;</h1>
