d60b7516bc2d06b2dea3f429201ac2abacc4a95d
Zach Drummond <zachdrummond3@gmail.com>
2022-01-20 16:07:37 -0500

    Updated navbar colors

--- src/Components/Icons/svg.jsx
+++ src/Components/Icons/svg.jsx
@@ -1,4 +1,4 @@
-const svg = ({ width, height, color, path }) => {
+const SVG = ({ width, height, color, children }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -7,9 +7,9 @@ const svg = ({ width, height, color, path }) => {
       viewBox="0 0 24 24"
       fill={color}
     >
-      {path}
+      {children}
     </svg>
   );
 };
 
-export default svg;
+export default SVG;
