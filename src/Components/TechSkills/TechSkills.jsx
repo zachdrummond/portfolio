2f9c8775c05d1c65933931e06fde7e667d401d28
Zach Drummond <zachdrummond3@gmail.com>
2022-02-05 19:52:36 -0500

    Set it back

--- src/Components/TechSkills/TechSkills.jsx
+++ src/Components/TechSkills/TechSkills.jsx
@@ -14,7 +14,7 @@ const TechSkills = () => {
   // RETURNS - A new object (React Element <div>)
   const objectMap = (object) => {
     return (
-      <div className="d-flex flex-wrap-reverse justify-content-center border border-5 border-danger">
+      <div className="d-flex justify-content-center border border-5 border-danger">
         {Object.keys(object).map((key) => {
           let { name, image, alt } = object[key];
           return (
@@ -33,9 +33,8 @@ const TechSkills = () => {
 
   return (
     <Section name="Technical Skills">
-      {/* {objectMap(IMAGES.Skills.TopRow)}
-      {objectMap(IMAGES.Skills.BottomRow)} */}
-      {objectMap(IMAGES.Skills)}
+      {objectMap(IMAGES.Skills.Row1)}
+      {objectMap(IMAGES.Skills.Row2)}
     </Section>
   );
 };
--- src/images/images.js
+++ src/images/images.js
@@ -47,7 +47,8 @@ const IMAGES = {
       images: {
         logo: require("./Projects/FindYourAdventure/HomePage.png"),
         one: require("./Projects/FindYourAdventure/ActivitiesPage.png"),
-        two: require("./Projects/FindYourAdventure/ListOfParksPage.png"),
+        two: require("./Projects/FindYourAdventure/ListOfParksPage.png")
+          ,
         three: require("./Projects/FindYourAdventure/ParkPage.png"),
       },
       alt: "Find Your Adventure Project",
@@ -64,7 +65,8 @@ const IMAGES = {
       images: {
         logo: require("./Projects/GoogleBooksSearch/HomePage.png"),
         one: require("./Projects/GoogleBooksSearch/SavedBooksPage.png"),
-        two: require("./Projects/GoogleBooksSearch/SearchResultsPage.png"),
+        two: require("./Projects/GoogleBooksSearch/SearchResultsPage.png")
+          ,
       },
       alt: "Google Books Search Project",
       link: "https://google-books-search-zd.herokuapp.com/search",
@@ -105,54 +107,54 @@ const IMAGES = {
     },
   },
   Skills: {
-    // TopRow: {
-    React: {
-      name: "REACT.JS",
-      image: require("./Skills/React.svg").default,
-      alt: "React Logo",
+    Row1: {
+      React: {
+        name: "REACT.JS",
+        image: require("./Skills/React.svg").default,
+        alt: "React Logo",
+      },
+      Nodejs: {
+        name: "NODE.JS",
+        image: require("./Skills/Nodejs.svg").default,
+        alt: "Nodejs Logo",
+      },
+      Expressjs: {
+        name: "EXPRESS.JS",
+        image: require("./Skills/Expressjs.svg").default,
+        alt: "Expressjs Logo",
+      },
+      MongoDB: {
+        name: "MONGO DB",
+        image: require("./Skills/MongoDB.svg").default,
+        alt: "MongoDB Logo",
+      },
     },
-    Nodejs: {
-      name: "NODE.JS",
-      image: require("./Skills/Nodejs.svg").default,
-      alt: "Nodejs Logo",
-    },
-    Expressjs: {
-      name: "EXPRESS.JS",
-      image: require("./Skills/Expressjs.svg").default,
-      alt: "Expressjs Logo",
-    },
-    MongoDB: {
-      name: "MONGO DB",
-      image: require("./Skills/MongoDB.svg").default,
-      alt: "MongoDB Logo",
-    },
-    // },
-    // BottomRow: {
-    HTML: {
-      name: "HTML",
-      image: require("./Skills/HTML.svg").default,
-      alt: "HTML Logo",
-    },
-    CSS: {
-      name: "CSS",
-      image: require("./Skills/CSS.svg").default,
-      alt: "CSS Logo",
-    },
-    Javascript: {
-      name: "JAVASCRIPT",
-      image: require("./Skills/Javascript.svg").default,
-      alt: "JavaScript Logo",
-    },
-    Git: {
-      name: "GIT",
-      image: require("./Skills/Git.svg").default,
-      alt: "Git Logo",
-    },
-    MySQL: {
-      name: "MYSQL",
-      image: require("./Skills/MySQL.svg").default,
-      alt: "MySQL Logo",
-      // },
+    Row2: {
+      HTML: {
+        name: "HTML",
+        image: require("./Skills/HTML.svg").default,
+        alt: "HTML Logo",
+      },
+      CSS: {
+        name: "CSS",
+        image: require("./Skills/CSS.svg").default,
+        alt: "CSS Logo",
+      },
+      Javascript: {
+        name: "JAVASCRIPT",
+        image: require("./Skills/Javascript.svg").default,
+        alt: "JavaScript Logo",
+      },
+      Git: {
+        name: "GIT",
+        image: require("./Skills/Git.svg").default,
+        alt: "Git Logo",
+      },
+      MySQL: {
+        name: "MYSQL",
+        image: require("./Skills/MySQL.svg").default,
+        alt: "MySQL Logo",
+      },
     },
   },
 };
