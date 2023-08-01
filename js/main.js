1fb5a85e85cc4e0ede7e4c0c6c61582438b79146
Zach Drummond <zachdrummond3@gmail.com>
2023-08-01 12:09:49 -0400

    removed swiper

--- js/main.js
+++ js/main.js
@@ -94,8 +94,6 @@ modalCloses.forEach((modalClose) => {
   });
 });
 
-/*==================== PORTFOLIO SWIPER  ====================*/
-
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
